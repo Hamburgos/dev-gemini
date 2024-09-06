a73200ca635fca023c11aaaf7de6a6584195fb43
Hamburgos <silashsantana@gmail.com>
2024-09-06 20:18:51 -0300

    Mais esportes

--- app.js
+++ app.js
@@ -1,58 +1,57 @@
-function pesquisar() {
-    // Seleciona a seção HTML onde os resultados serão exibidos
-    let section = document.getElementById("resultados-pesquisa");
-    let campoPesquisa = document.getElementById("campo-pesquisa").value;
-    if (campoPesquisa == "") {
-        section.innerHTML = "Nenhum Item Encontrado"
-        return
+let dados = [
+    {
+        titulo: "<b>Águas Abertas</b>",
+        descricao: "<b>Descrição</b>: A natação em águas abertas é uma modalidade olímpica que desafia atletas a percorrer longas distâncias em águas naturais, como oceanos ou lagos. Os nadadores enfrentam condições como correntes marítimas e ondas, exigindo resistência e habilidades de navegação.",
+        historia_olimpica: "<b>História nas Olímpiadas</b>: A natação em águas abertas foi introduzida nos Jogos Olímpicos como esporte demonstrativo em 2008 e se tornou modalidade oficial em 2010. A prova de 10km é a única disputada nas Olimpíadas.",
+        regras: "<b>Regras</b>: As provas são realizadas em circuitos marcados por boias, e os nadadores devem seguir o percurso estabelecido. As regras incluem a proibição de contato físico e o uso de trajes de banho específicos.",
+        modalidades: "<b>Modalidades</b>: A única modalidade disputada nas Olimpíadas é a prova de 10km.",
+        estilos: "<b>Estilos</b>: Geralmente, apenas o crawl é utilizado em águas abertas.",
+        medalhistas_brasileiros:"<b>Medalhistas Brasileiros</b>: Ana Marcela Cunha - Ouro - 2020 | Allan do Carmo - Bronze - 2016 | Poliana Okimoto - Bronze - 2012",
+        link: "https://www.youtube.com/watch?v=coUW42nv4S8&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=11", // Vídeo com informações sobre o esporte
+        tags: "aguas"
+    },
+    {
+        titulo: "Atletismo",
+        descricao: "<b>Descrição</b>: O atletismo é um dos esportes mais antigos e completos dos Jogos Olímpicos, englobando diversas provas de corrida, saltos e arremessos.",
+        historia_olimpica: "<b> História nas Olímpiadas</b>: O atletismo está presente desde a primeira edição dos Jogos Olímpicos da era moderna, em Atenas 1896. As provas iniciais incluíam corridas de curta e longa distância, saltos e arremessos.",
+        regras: "<b>Regras</b>: As regras do atletismo são definidas pela World Athletics e variam de acordo com cada prova, mas geralmente envolvem a medição precisa de tempo, distância ou altura.",
+        modalidades: "<b>Modalidades</b>: Corrida, Salto, Arremesso",
+        estilos: "<b> Modalidades da Corrida</b>: Velocidade - Meio Fundo - Fundo | <b>Modalidades do Salto</b>: Em Altura - Em Distância - Com Vara | <b>Modalidades Arremessso</b>: Peso - Disco - Martelo - Dardo",
+        medalhistas_brasileiros: "<b>Medalhistas Brasileiros</b>: Alison dos Santos - Bronze - 2020 - 400m com barreiras",
+        link: "https://www.youtube.com/watch?v=CaaGrb8l_7E&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=2", // Vídeo com informações sobre o esporte
+        tags: "atletimo atetismo"
+    },
+    {
+        titulo: "Badminton",
+        descricao: "<b>Descrição</b>: O badminton é um esporte de raquete que combina habilidade, velocidade e estratégia. Os jogadores usam raquetes para bater em um volante sobre uma rede.",
+        historia_olimpica: "<b> História nas Olímpiadas</b>: O badminton foi introduzido nos Jogos Olímpicos de Barcelona 1992. Rapidamente se tornou um dos esportes mais populares dos Jogos.",
+        regras: "<b>Regras</b>: As regras do badminton envolvem servir o volante por baixo da cintura, bater no volante apenas uma vez por vez e evitar que ele caia no seu campo.",
+        modalidades: "<b>Modalidades</b>: Singles, Duplas",
+        estilos:"<b>Estilos</b>: No badminton, os estilos são mais relacionados às diferentes jogadas e estratégias do que a um nome específico | Jogadas: Smash, Drop Shot, Lob, Clear",
+        medalhistas_brasileiros: "<b>Medalhistas Brasileiros</b>: Infelizmente, o Brasil ainda não conquistou medalhas olímpicas no Badminton.",
+        link: "https://www.youtube.com/watch?v=uNYqtEXkFGg&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=29", // Substitua pelo link desejado
+        tags: "badmiton badmington"
+    },
+    {
+        titulo: "Basquetebol",
+        descricao: "<b>Descrição</b>: O basquetebol é um esporte de equipe que combina habilidade, força e estratégia. Os jogadores tentam marcar pontos lançando uma bola através de uma cesta.",
+        historia_olimpica: "<b> História nas Olímpiadas</b>: O basquetebol foi introduzido nos Jogos Olímpicos de Berlim 1936. Desde então, tornou-se um dos esportes mais populares dos Jogos.",
+        regras: "<b>Regras</b>: As regras do basquetebol envolvem driblar a bola, passar para os companheiros de equipe e marcar pontos através de cestas. Faltas e violações são penalizadas.",
+        modalidades: "<b>Modalidades</b>: Masculino, Feminino",
+        estilos: "<b>Estilos</b>: No basquete, os estilos são mais relacionados a posições dos jogadores do que a um nome específico | <b/>Posições</b>: Armador, Ala, Pivô",
+        medalhistas_brasileiros: "<b>Medalhistas Brasileiros</b>: Infelizmente, o Brasil ainda não conquistou medalhas olímpicas no Basquetebol.",
+        link: "https://www.youtube.com/watch?v=IA8A869mjeI&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=32", // Substitua pelo link desejado
+        tags: "basquete basqueti"
+    },
+    {
+        titulo: "Boxe",
+        descricao: "<b>Descrição</b>: O boxe é um esporte de combate em que dois atletas usam luvas para se enfrentar em um ringue. O objetivo é conectar golpes no adversário para pontuar ou nocauteá-lo.",
+        historia_olimpica: "<b> História nas Olímpiadas</b>: O boxe esteve presente em quase todas as edições dos Jogos Olímpicos da era moderna, desde Atenas 1896. No entanto, passou por diversas interrupções e mudanças de formato ao longo dos anos.",
+        regras: "<b>Regras</b>: As regras do boxe envolvem a utilização de luvas, a proibição de golpes abaixo da cintura e a contagem de pontos por golpes conectados na área do corpo legal.",
+        modalidades:"Modalidades</b>: masculino, feminino",
+        estilos: "<b>Estilos</b>: No boxe, os estilos são mais relacionados às diferentes guardas e técnicas de luta do que a um nome específico | <b>Técnicas de Luta</b>: Ortogonal, Sulca, Filipino",
+        medalhistas_brasileiros: "<b>Medalhistas Brasileiros</b>: Hebert Conceição - Ouro - 2020 - Categoria 75 Kg nos Jogos Olímpicos de Tóquio 2020 na categoria 75 Kg | Bia Ferreira - Prata - 2020 - Categoria 75 Kg | Abner Teixeira -  Bronze - 2020 - Categoria 91 Kg",
+        link: "https://www.youtube.com/watch?v=9hVSjR_3aRE&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=1", // Substitua pelo link desejado
+        tags: "luta sair na mão"
     }
-    campoPesquisa = campoPesquisa.toLowerCase()
-    console.log(campoPesquisa);
-    console.log(section); // Linha para depuração, verifica se a seção foi encontrada
-  
-    // Inicializa uma string vazia para armazenar os resultados
-    let resultados = "";
-    let titulo = "";
-    let descricao = "";
-    let historia_olimpica = "";
-    let regras = "";
-    let modalidades = "";
-    let estilos = "";
-    let medalhistas_brasileiros = "";
-    let tags = "";
-  
-    // Itera sobre cada item de dados e constrói o HTML
-    for (let dado of dados) {
-      titulo = dado.titulo.toLocaleLowerCase()
-      descricao = dado.descricao.toLocaleLowerCase()
-      historia_olimpica = dado.historia_olimpica.toLocaleLowerCase()
-      regras = dado.regras.toLocaleLowerCase()
-      modalidades = dado.modalidades.toLocaleLowerCase()
-      estilos = dado.estilos.toLocaleLowerCase()
-      medalhistas_brasileiros = dado.medalhistas_brasileiros.toLocaleLowerCase()
-      tags = dado.tags.toLocaleLowerCase()
-      // se titulo inclues campoPesquisa
-      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || historia_olimpica.includes(campoPesquisa) || regras.includes(campoPesquisa) || modalidades.includes(campoPesquisa) || estilos.includes(campoPesquisa) || medalhistas_brasileiros.includes(campoPesquisa) || tags.includes(campoPesquisa)){
-        // cria um novo elemento
-        resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <p class="descricao-meta">${dado.historia_olimpica}</p>
-          <p class="descricao-meta">${dado.regras}</p>
-          <p class="descricao-meta">${dado.modalidades}</p>
-          <p class="descricao-meta">${dado.estilos}</p>
-          <p class="descricao-meta">${dado.medalhistas_brasileiros}</p>
-          <a href="${dado.link}" target="_blank"><b>Mais informações sobre o esporte</b></a>
-        </div>
-      `
-      }
-      // Cria um elemento div para cada item e adiciona informações como título, descrição e link
-      
-    }
-  
-    // Atribui o HTML gerado para a seção, substituindo o conteúdo anterior
-    section.innerHTML = resultados
-  }
+];
