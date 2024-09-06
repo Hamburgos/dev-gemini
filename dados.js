e1ed79a3c1fde22f811a39c324b62df9fae765ea
Hamburgos <silashsantana@gmail.com>
2024-09-05 21:31:20 -0300

    Adicionando mais esportes

--- dados.js
+++ dados.js
@@ -1,51 +1,22 @@
 let dados = [
     {
-        titulo: "Tênis",
-        descricao: "As regras do tênis nas Olimpíadas seguem, em sua maioria, as mesmas regras aplicadas em outros torneios profissionais. No entanto, algumas particularidades e formatos específicos são adotados para se adequar ao formato dos Jogos Olímpicos",
-        link: "https://www.youtube.com/watch?v=crtAMYQIO9A&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=23"
+        titulo: "<b>Natação em Águas Abertas</b>",
+        descricao: "<b>Descrição</b>: A natação em águas abertas é uma modalidade olímpica que desafia atletas a percorrer longas distâncias em águas naturais, como oceanos ou lagos. Os nadadores enfrentam condições como correntes marítimas e ondas, exigindo resistência e habilidades de navegação.",
+        historia_olimpica: "<b>História nas Olímpiadas</b>: A natação em águas abertas foi introduzida nos Jogos Olímpicos como esporte demonstrativo em 2008 e se tornou modalidade oficial em 2010. A prova de 10km é a única disputada nas Olimpíadas.",
+        regras: "<b>Regras</b>: As provas são realizadas em circuitos marcados por boias, e os nadadores devem seguir o percurso estabelecido. As regras incluem a proibição de contato físico e o uso de trajes de banho específicos.",
+        modalidades: "<b>Modalidades</b>: A única modalidade disputada nas Olimpíadas é a prova de 10km.",
+        estilos: "<b>Estilos</b>: Geralmente, apenas o crawl é utilizado em águas abertas.",
+        medalhistas_brasileiros:"<b>Medalhistas Brasileiros</b>: Ana Marcela Cunha - Ouro - 2020 | Allan do Carmo - Bronze - 2016 | Poliana Okimoto - Bronze - 2012",
+        link: "https://www.youtube.com/watch?v=coUW42nv4S8&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=11" // Vídeo com informações sobre o esporte
     },
     {
-        titulo: "Natação",
-        descricao: "A natação olímpica é uma das modalidades mais populares dos Jogos Olímpicos. Os atletas competem em diversas provas, como estilo livre, costas, peito e borboleta, em diferentes distâncias.",
-        link: "https://www.youtube.com/watch?v=Ugr5gtCBuoU&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=3",
-        modalidade: "individual",
-        estilos: ["crawl", "costas", "peito", "borboleta"],
-        distancias: ["50m", "100m", "200m", "400m", "800m", "1500m"],
-        regras_especificas: "As provas de revezamento são uma característica marcante da natação olímpica."
-    },
-    {
-        titulo: "Judô",
-        descricao: "O judô olímpico é uma arte marcial e esporte de combate que valoriza a técnica e a estratégia. Os atletas buscam projeções, imobilizações e estrangulamentos para vencer seus adversários.",
-        link: "https://www.youtube.com/watch?v=6LWfTDfOm7I&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=4",
-        modalidade: "individual",
-        categorias_de_peso: ["leve", "meio-leve", "leve", "médio", "meio-médio", "meio-pesado", "pesado"],
-        tecnicas_fundamentais: ["ippon", "waza-ari", "yuko"],
-        regras_especificas: "O judô olímpico utiliza um sistema de pontuação para determinar o vencedor.",
-        fundador: "Jigoro Kano"
-    },
-    {
-        titulo: "Ginástica Rítmica",
-        descricao: "A ginástica rítmica é uma modalidade artística que combina elementos de dança, ginástica e manipulação de aparelhos. As atletas executam rotinas com música, demonstrando flexibilidade, força e graça.",
-        link: "https://www.youtube.com/watch?v=DTTdE6Zgcw8&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=12",
-        modalidade: "individual e em grupo",
-        aparelhos: "bola, arco, maça, fita e bastões"
-    },
-    {
-        titulo: "Futebol",
-        descricao: "O futebol é o esporte mais popular do mundo e uma das principais modalidades dos Jogos Olímpicos. As equipes disputam partidas emocionantes em busca da medalha de ouro.",
-        link: "https://www.youtube.com/watch?v=0gIjkIcuygQ&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=7",
-        modalidade: "equipe",
-        formato: "11 contra 11"
-    },
-    {
-        titulo: "Boxe",
-        descricao: "O boxe olímpico é uma arte marcial e esporte de combate que exige força, agilidade e técnica. Os atletas utilizam luvas e protetores bucais para minimizar os riscos de lesões.",
-        link: "https://www.youtube.com/watch?v=9hVSjR_3aRE&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c",
-        modalidade: "individual",
-        categorias_de_peso: ["peso mosca", "peso galo", "peso pena", "peso leve", "peso meio-médio", "peso médio", "peso meio-pesado", "peso pesado"],
-        proteções: ["luvas", "protetor bucal"],
-        regras_especificas: "As lutas são divididas em rounds e os pontos são marcados por golpes eficazes. O objetivo é nocautear o adversário ou ter a maior pontuação ao final da luta.",
-        origem: "Grécia Antiga",
-        organização: "Associação Internacional de Boxe Amateur (AIBA)"
-    },
+        titulo: "Atletismo",
+        descricao: "Descrição: O atletismo é um dos esportes mais antigos e completos dos Jogos Olímpicos, englobando diversas provas de corrida, saltos e arremessos.",
+        historia: "<b> História nas Olímpiadas</b>:O atletismo está presente desde a primeira edição dos Jogos Olímpicos da era moderna, em Atenas 1896. As provas iniciais incluíam corridas de curta e longa distância, saltos e arremessos.",
+        regras: "<b>Regras</b>: As regras do atletismo são definidas pela World Athletics e variam de acordo com cada prova, mas geralmente envolvem a medição precisa de tempo, distância ou altura.",
+        modalidades: "<b>Modalidades</b>: corrida, salto, arremesso",
+        estilos: "<b> Modalidades da Corrida</b>: velocidade - meio fundo - fundo | <b>Modalidades do Salto</b>: em altura - em distância - com vara | <b>Modalidades Arremessso</b>: peso - disco - martelo - dardo",
+        medalhistas_brasileiros: "<b>Medalhistas Brasileiros</b>: Alison dos Santos - Bronze - 2020 - 400m com barreiras",
+        link: "https://www.youtube.com/watch?v=CaaGrb8l_7E&list=PL1c-gPLYY8YwU45PxUHOZyXnsuJa-rF5c&index=2" // Vídeo com informações sobre o esporte
+    }
 ];
